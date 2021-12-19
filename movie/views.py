9e26b8cd718e7d207d3ae3832f06ae1b3a5db736
victor-s-santos <victor.santos.cd@gmail.com>
2021-12-19 16:28:11 -0300

    removing unecessary print

--- movie/views.py
+++ movie/views.py
@@ -1,24 +1,28 @@
 from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
 from movie.models import Category, Movie
 from movie.serializers import CategorySerializer, MovieSerializer
-from rest_framework.response import Response
+
 
 class MovieListCreateView(generics.ListCreateAPIView):
     serializer_class = MovieSerializer
-    
+
     def get(self, request):
         movies = Movie.objects.all()
         serializer_class = MovieSerializer(movies, many=True)
         return Response(serializer_class.data, status=status.HTTP_200_OK)
 
+
 class CategoryListCreateView(generics.ListCreateAPIView):
     serializer_class = CategorySerializer
-    
+
     def get(self, request):
         categories = Category.objects.all()
         serializer_class = CategorySerializer(categories, many=True)
         return Response(serializer_class.data, status=status.HTTP_200_OK)
-    
+
     def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
