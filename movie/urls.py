2949d2a0c1b63d70e73f94bbdd391e23bba2c141
victor-s-santos <victor.santos.cd@gmail.com>
2021-12-19 19:28:33 -0300

    create detail endpoint

--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from movie.views import MovieListCreateView, CategoryListCreateView
+
+from movie.views import (
+    CategoryListCreateView,
+    MovieListCreateView,
+    MovieRetrieveUpdateDestroyView,
+)
 
 
 urlpatterns = [
-    path('', MovieListCreateView.as_view(), name='movies'),
-    path('category/', CategoryListCreateView.as_view(), name='categories'),
+    path("", MovieListCreateView.as_view(), name="movies"),
+    path(
+        "<int:movie_id>/",
+        MovieRetrieveUpdateDestroyView.as_view(),
+        name="movie_detail",
+    ),
+    path("category/", CategoryListCreateView.as_view(), name="categories"),
 ]
