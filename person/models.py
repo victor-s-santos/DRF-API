6c829e17387abf5208af869bc432227c72143dc6
victor-s-santos <victor.santos.cd@gmail.com>
2021-12-30 16:55:45 -0300

    create score_average in Author class model from person models

--- person/models.py
+++ person/models.py
@@ -1,19 +1,28 @@
 from django.db import models
 
+
 class Author(models.Model):
-    GENRE_CHOICES = (
-        (1, "Female"), (2, "Male")
-        )
+    GENRE_CHOICES = ((1, "Female"), (2, "Male"))
     name = models.CharField(max_length=200)
     genre = models.IntegerField(choices=GENRE_CHOICES, default=1)
     birth_date = models.DateField()
     nationality = models.CharField(max_length=200)
 
-    
+    @property
+    def score_average(self):
+        """average score of the films this author worked on"""
+        author = Author.objects.get(id=self.id)
+        try:
+            movies = author.movie_set.all()
+            list_score = [movie.score for movie in movies]
+            avg_score = sum(list_score) / len(list_score)
+            return avg_score
+        except Exception as e:
+            return "An error occured! {e}"
+
+
 class Actor(models.Model):
-    GENRE_CHOICES = (
-        (1, "Female"), (2, "Male")
-        )
+    GENRE_CHOICES = ((1, "Female"), (2, "Male"))
     name = models.CharField(max_length=200)
     genre = models.IntegerField(choices=GENRE_CHOICES, default=1)
     birth_date = models.DateField()
