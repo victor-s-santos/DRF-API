f990800dc0929ca851c6c34e30e8e1e1f9daf583
victor-s-santos <victor.santos.cd@gmail.com>
2022-01-02 11:15:05 -0300

    test the full flow to post a category

--- movie/tests/test_views.py
+++ movie/tests/test_views.py
@@ -1,15 +1,48 @@
 from django.urls import reverse
 
 import pytest
+from _pytest.fixtures import fixture
 
 
 path_list_200 = [("movies", 200), ("categories", 200)]
+
+path_list_empty_content = [
+    ("movies", "There is no movie in the database!"),
+    ("categories", "There is no category in the database!"),
+]
+
+path_list_content = [("categories", "Terror")]
 
 
 class Test_get_request:
     @pytest.mark.django_db
     @pytest.mark.parametrize("path, status_code", path_list_200)
-    def test_get_actor_author_endpoints(self, api_client, path, status_code):
+    def test_get_movie_endpoints(self, api_client, path, status_code):
         """Must return status_code 200"""
         url = reverse(path)
         assert api_client.get(url).status_code == status_code
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("path, status_code", path_list_empty_content)
+    def test_get_movie_content(self, api_client, path, status_code):
+        """Must return status_code 200"""
+        url = reverse(path)
+        assert api_client.get(url).data == "There is no movie in the database!"
+
+    @fixture
+    @pytest.mark.django_db
+    def test_post_category(self, api_client):
+        url = reverse("categories")
+        data = {"category_name": "Terror"}
+        response = api_client.post(url, data)
+        assert response.status_code == 201
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("path, content", path_list_content)
+    def test_get_category_content(
+        self, api_client, path, content, test_post_category
+    ):
+        """Must return status_code 200"""
+        url = reverse(path)
+        data = [{"category_name": content}]
+        assert api_client.get(url).data == data
