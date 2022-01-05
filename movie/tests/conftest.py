166ad882e3e175ff91aa063ff952d4d5c3c2b848
victor-s-santos <victor.santos.cd@gmail.com>
2022-01-05 19:11:03 -0300

    define conftest

--- movie/tests/conftest.py
+++ movie/tests/conftest.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
 import pytest
+
+from movie.models import Category
+from person.models import Actor, Author
 
 
 @pytest.fixture
@@ -6,3 +11,34 @@ def api_client():
     from rest_framework.test import APIClient
 
     return APIClient()
+
+
+@pytest.fixture
+def actor_obj():
+    actor = Actor.objects.create(
+        name="Actor Test",
+        genre=1,
+        birth_date=datetime.now(),
+        nationality="Pythonic",
+    )
+    actor.save()
+    return actor
+
+
+@pytest.fixture
+def author_obj():
+    author = Author.objects.create(
+        name="Author Test",
+        genre=1,
+        birth_date=datetime.now(),
+        nationality="Pythonic",
+    )
+    author.save()
+    return author
+
+
+@pytest.fixture
+def category_obj():
+    category = Category.objects.create(category_name="Category Test")
+    category.save()
+    return category
