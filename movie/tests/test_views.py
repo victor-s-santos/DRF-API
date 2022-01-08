4bdd4f0f04a44b14983a7e0221acadb0cb636dbf
victor-s-santos <victor.santos.cd@gmail.com>
2022-01-08 16:10:24 -0300

    recovering test_views

--- movie/tests/test_views.py
+++ movie/tests/test_views.py
@@ -1,210 +1,72 @@
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.serializers import Serializer
+from datetime import date
 
-from movie.models import Category, Movie
-from movie.serializers import (
-    CategorySerializer,
-    MovieDetailSerializer,
-    MovieSerializer,
-)
+from django.urls import reverse
+
+import pytest
+
+from person.models import Actor, Author
 
 
-def verify_request(request):
-    if request.method == "POST":
-        if (
-            "title" not in request.data
-            or "category" not in request.data
-            or "synopsis" not in request.data
-            or "publication_date" not in request.data
-            or "main_actor" not in request.data
-            or "main_author" not in request.data
-        ):
-            return False
-        return True
-    if request.method == "PATCH":
-        if (
-            "title" not in request.data
-            and "category" not in request.data
-            and "synopsis" not in request.data
-            and "publication_date" not in request.data
-            and "main_actor" not in request.data
-            and "main_author" not in request.data
-        ):
-            return False
-        return True
+path_list_200 = [("movies", 200), ("categories", 200)]
+
+path_list_empty_content = [
+    ("movies", "There is no movie in the database!"),
+    ("categories", "There is no category in the database!"),
+]
+
+path_list_content = [("categories", "Category Test")]
 
 
-class MovieListCreateView(generics.ListCreateAPIView):
-    serializer_class = MovieSerializer
+class Test_get_request:
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("path, status_code", path_list_200)
+    def test_get_movie_endpoints(self, api_client, path, status_code):
+        """Must return status_code 200"""
+        url = reverse(path)
+        assert api_client.get(url).status_code == status_code
 
-    def post(self, request) -> Response:
-        if verify_request(request):
-            serializer = MovieSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
-        else:
-            return Response(
-                f"Invalid Request!", status=status.HTTP_400_BAD_REQUEST
-            )
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("path, status_code", path_list_empty_content)
+    def test_get_movie_content(self, api_client, path, status_code):
+        """Must return status_code 200"""
+        url = reverse(path)
+        assert api_client.get(url).data == "There is no movie in the database!"
 
-    def get(self, request) -> Response:
-        movies = Movie.objects.all()
-        if len(movies) == 0:
-            return Response(
-                "There is no movie in the database!", status=status.HTTP_200_OK
-            )
-        serializer_class = MovieSerializer(movies, many=True)
-        return Response(serializer_class.data, status=status.HTTP_200_OK)
+    @pytest.mark.django_db
+    def test_post_category(self, api_client):
+        url = reverse("categories")
+        data = {"category_name": "Terror"}
+        response = api_client.post(url, data)
+        assert response.status_code == 201
 
+    @pytest.mark.django_db
+    @pytest.mark.parametrize("path, content", path_list_content)
+    def test_get_category_content(
+        self, api_client, path, content, category_obj
+    ):
+        """Must return status_code 200"""
+        url = reverse(path)
+        data = [{"category_name": content}]
+        assert api_client.get(url).data == data
 
-class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = MovieDetailSerializer
-
-    def get(self, request, movie_id: int = None) -> Response:
-        try:
-            movie_detail = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            return Response(
-                "Movie does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
-        serializer_class = MovieDetailSerializer(movie_detail, many=False)
-        return Response(serializer_class.data, status=status.HTTP_200_OK)
-
-    def patch(self, request, movie_id: int = None) -> Response:
-        if not verify_request(request):
-            return Response(
-                f"No field has been informed!",
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        try:
-            movie_detail = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            return Response(
-                f"Movie does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
-        serializer_class = MovieDetailSerializer(
-            movie_detail, data=request.data, partial=True
+    @pytest.mark.django_db
+    def test_post_movie(self, api_client, category_obj, actor_obj, author_obj):
+        url = reverse("movies")
+        data = {
+            "title": "Title test",
+            "category": category_obj.id,
+            "synopsis": "Test synopsis",
+            "main_author": author_obj.id,
+            "main_actor": actor_obj.id,
+            "publication_date": date.today(),
+        }
+        response = api_client.post(url, data)
+        assert response.status_code == 201
+        assert (
+            Actor.objects.get(id=response.data["main_actor"][0]).name
+            == "Actor Test"
         )
-        if serializer_class.is_valid():
-            serializer_class.save()
-            return Response(
-                serializer_class.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer_class.errors, status=status.HTTP_400_BAD_REQUEST
+        assert (
+            Author.objects.get(id=response.data["main_author"][0]).name
+            == "Author Test"
         )
-
-    def delete(self, request, movie_id: int = None) -> Response:
-        try:
-            movie_detail = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            return Response(
-                f"Movie does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            movie_detail.delete()
-            return Response(
-                f"Movie has been deleted successfully!",
-                status=status.HTTP_200_OK,
-            )
-        except Exception as e:
-            return Response(
-                f"An error occured {e}!", status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-class CategoryListCreateView(generics.ListCreateAPIView):
-    serializer_class = CategorySerializer
-
-    def get(self, request) -> Response:
-        categories = Category.objects.all()
-        if len(categories) == 0:
-            return Response(
-                "There is no movie in the database!", status=status.HTTP_200_OK
-            )
-        serializer_class = CategorySerializer(categories, many=True)
-        return Response(serializer_class.data, status=status.HTTP_200_OK)
-
-    def post(self, request) -> Response:
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class AddActorToMovieView(generics.CreateAPIView):
-    serializer_class = MovieSerializer
-
-    def post(self, request, movie_id: int = None) -> Response:
-        try:
-            movie = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            return Response(
-                "Movie does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            actor_list = request.data["actor_list"]
-            type(actor_list) == list
-        except AssertionError as e:
-            return Response(
-                f"Invalid data type: {e}!", status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            self.add_actor(movie_id=movie_id, actor_list=actor_list)
-            return Response(
-                f"Actors {actor_list} has been added successfully to {movie.title}"
-            )
-        except Exception as e:
-            return Response(
-                f"An exception occured {e}!", status=status.HTTP_400_BAD_REQUEST
-            )
-
-    @classmethod
-    def add_actor(cls, movie_id: int, actor_list: list) -> bool:
-        movie = Movie.objects.get(id=movie_id)
-        for actor in actor_list:
-            movie.main_actor.add(actor)
-            movie.save()
-        return True
-
-
-class AddAuthorToMovieView(generics.CreateAPIView):
-    serializer_class = MovieSerializer
-
-    def post(self, request, movie_id: int = None) -> Response:
-        try:
-            movie = Movie.objects.get(id=movie_id)
-        except Movie.DoesNotExist:
-            return Response(
-                "Movie does not exist!", status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            author_list = request.data["author_list"]
-            type(author_list) == list
-        except AssertionError as e:
-            return Response(
-                f"Invalid data type: {e}!", status=status.HTTP_400_BAD_REQUEST
-            )
-        try:
-            self.add_author(movie_id=movie_id, author_list=author_list)
-            return Response(
-                f"Actors {author_list} has been added successfully to {movie.title}"
-            )
-        except Exception as e:
-            return Response(
-                f"An exception occured {e}!", status=status.HTTP_400_BAD_REQUEST
-            )
-
-    @classmethod
-    def add_author(cls, movie_id: int, author_list: list) -> bool:
-        movie = Movie.objects.get(id=movie_id)
-        for author in author_list:
-            movie.main_author.add(author)
-            movie.save()
-        return True
