f6d6f3deb76a2a31ba001c5acad08e98b3af0dbf
victor-s-santos <victor.santos.cd@gmail.com>
2022-01-17 20:06:38 -0300

    add more entities in conftest to use in view tests

--- movie/tests/conftest.py
+++ movie/tests/conftest.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import date, datetime
 
 import pytest
 
-from movie.models import Category
+from movie.models import Category, Movie
 from person.models import Actor, Author
 
 
@@ -26,6 +26,18 @@ def actor_obj():
 
 
 @pytest.fixture
+def actor_obj2():
+    actor = Actor.objects.create(
+        name="Actor Test2",
+        genre=1,
+        birth_date=datetime.now(),
+        nationality="Pythonic",
+    )
+    actor.save()
+    return actor
+
+
+@pytest.fixture
 def author_obj():
     author = Author.objects.create(
         name="Author Test",
@@ -38,7 +50,39 @@ def author_obj():
 
 
 @pytest.fixture
+def author_obj2():
+    author = Author.objects.create(
+        name="Author Test2",
+        genre=1,
+        birth_date=datetime.now(),
+        nationality="Pythonic",
+    )
+    author.save()
+    return author
+
+
+@pytest.fixture
 def category_obj():
     category = Category.objects.create(category_name="Category Test")
+    category.save()
+    return category
+
+
+@pytest.fixture
+def movie_obj(category_obj, actor_obj, author_obj):
+    movie = Movie.objects.create(
+        title="Movie Test",
+        category=category_obj,
+        synopsis="Synopsis Test",
+        publication_date=date.today(),
+    )
+    movie.main_actor.add(actor_obj.id)
+    movie.main_author.add(author_obj.id)
+    return movie
+
+
+@pytest.fixture
+def category_obj2():
+    category = Category.objects.create(category_name="Category Test 2")
     category.save()
     return category
